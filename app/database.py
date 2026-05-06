4bf049c8e2a7e5ba32eaf41a84fa379ee4dda302
marc wendt <mail@marcwendt.com>
2026-05-06 11:24:34 +0200

    Datenbankkonfiguration mit SQLAlchemy hinzugefügt

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+from app.core.config import settings
+
+engine = create_engine(settings.DATABASE_URL, check_same_thread=False)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+class Base(DeclarativeBase):
+    pass
